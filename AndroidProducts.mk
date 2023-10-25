34f1983a935ba34582faad670883805be4b518d6
Greg Kaiser <gkaiser@google.com>
2023-10-25 16:44:34 -0600

    Fix lunch choices for git_main
    We add "trunk_staging-" for all COMMON_LUNCH_CHOICES so they are
    valid in the trunk stable setup.
    
    Bug: 307738446
    Test: lunch

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -10,6 +10,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_rpi5_tv.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_rpi5-userdebug \
-    aosp_rpi5_car-userdebug \
-    aosp_rpi5_tv-userdebug
+    aosp_rpi5-trunk_staging-userdebug \
+    aosp_rpi5_car-trunk_staging-userdebug \
+    aosp_rpi5_tv-trunk_staging-userdebug
