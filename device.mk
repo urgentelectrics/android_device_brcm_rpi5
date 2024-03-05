af69bfa76c0e36e02f39b1ef4e30dd17126c62b6
Konsta <konsta09@gmail.com>
2024-03-05 14:48:30 +0200

    seccomp_policy: remove v4l2_codec2
    * Fixes 32979cf0ba54da76a02663d54ef6c2fc3ecff2bf.

--- device.mk
+++ device.mk
@@ -234,7 +234,6 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/seccomp_policy/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(DEVICE_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(DEVICE_PATH)/seccomp_policy/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
